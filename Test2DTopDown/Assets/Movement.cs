3d60deb3345eb3550dd5e50af6990ee6e67a22e7
essarrgee <55785294+essarrgee@users.noreply.github.com>
2019-10-03 01:58:22 -0700

    Add Player Character
    -Add player sprite
    -Add RigidBody2D into player object
    -Add basic movement script into player object

---
+++ Test2DTopDown/Assets/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	public float speed;
+	public Rigidbody2D rb;
+	Vector2 inputdir;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		speed = 5f;
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputdir.x = Input.GetAxis("Horizontal");
+		inputdir.y = Input.GetAxis("Vertical");
+    }
+	
+	void FixedUpdate()
+	{
+		//rb.transform.position = rb.transform.position+inputdir*speed;
+		rb.MovePosition(rb.position+inputdir*speed*Time.fixedDeltaTime);
+	}
+}
